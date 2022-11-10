1f26a86d2d79537c2f1b7630d6bdb93d208fc4e9
Havhar0 <112910346+Havhar0@users.noreply.github.com>
2022-11-10 14:46:21 +0000

    All basic funcionalities added

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+let color = "black"
+
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(32);
+
+    let btnPopup = document.querySelector("#btn-popup");
+
+    btnPopup.addEventListener('click', function() {
+       let size = whatSize();
+       createBoard(size);
+    })
+
 })
 
 function createBoard(size) {
@@ -13,9 +23,41 @@ function createBoard(size) {
 
    for(let i=0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "grey";
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
-    
+}
 
+function whatSize() {
+
+    let gridChoice = prompt("Please enter size of the board");
+    let message = document.querySelector("#message");
+    if ( gridChoice == "" ) {
+        message.innerHTML = "Please enter a number";
+    } 
+    else if ( gridChoice < 0 || gridChoice > 100 ) {
+        message.innerHTML = "Please provide a number between 0 and 100"
+    }
+    else {
+        message.innerHTML = "Thank you"
+    }
+    return gridChoice;
+}
+
+function colorDiv() {
+    if ( color == 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else {
+        this.style.backgroundColor = "black"
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll("div")
+    divs.forEach( (div) => div.style.backgroundColor = "white" )
 }
